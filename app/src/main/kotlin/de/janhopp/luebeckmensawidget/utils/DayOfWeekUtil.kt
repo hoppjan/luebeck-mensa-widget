8b1398e9e451d0e8ca4533e621798656e69eed8e
Importantus <Betterplayer01@gmail.com>
2023-10-16 19:06:09 +0200

    Change days for api to english

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/utils/DayOfWeekUtil.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/utils/DayOfWeekUtil.kt
@@ -3,14 +3,13 @@ package de.janhopp.luebeckmensawidget.utils
 import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.DayOfWeek.*
 
-// this is German... @importantus & @draculente
 fun DayOfWeek.toMensaApiFormat() = when (this) {
-    MONDAY -> "mo&week=current"
-    TUESDAY -> "di&week=current"
-    WEDNESDAY -> "mi&week=current"
-    THURSDAY -> "do&week=current"
-    FRIDAY -> "fr&week=current"
-    else -> "mo&week=next" // no service at weekends
-    /*SATURDAY -> "sa"
-    SUNDAY -> "so"*/
+    MONDAY -> "mon&week=current"
+    TUESDAY -> "tue&week=current"
+    WEDNESDAY -> "wed&week=current"
+    THURSDAY -> "thu&week=current"
+    FRIDAY -> "fri&week=current"
+    else -> "mon&week=next" // no service at weekends
+    /*SATURDAY -> "sat"
+    SUNDAY -> "sun"*/
 }
