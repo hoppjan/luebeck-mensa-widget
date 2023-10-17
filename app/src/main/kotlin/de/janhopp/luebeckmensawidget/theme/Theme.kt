0da63a84d849defe4ad77db870801259f37517c4
Leonard-Orlando Menzel <79226837+lomenzel@users.noreply.github.com>
2023-10-17 11:57:16 +0200

    Fixed dark mode (#12)

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/theme/Theme.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/theme/Theme.kt
@@ -4,10 +4,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.glance.LocalContext
+import androidx.glance.color.isNightMode
 
 @Composable
 fun AppTheme(
-    useDarkTheme: Boolean = true,
+    useDarkTheme: Boolean = LocalContext.current.isNightMode,
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
