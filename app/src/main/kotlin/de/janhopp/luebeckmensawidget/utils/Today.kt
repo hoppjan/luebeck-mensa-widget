c305f5ceaf042091b59b50a555acfbd57354f52a
Leonard-Orlando Menzel <79226837+lomenzel@users.noreply.github.com>
2023-10-17 12:30:49 +0200

    Changed day to next day once mensa is closed (#15)

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/utils/Today.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/utils/Today.kt
@@ -1,14 +1,22 @@
 package de.janhopp.luebeckmensawidget.utils
 
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
 
 val currentTime: LocalDateTime
     get() = Clock.System.now()
         .toLocalDateTime(TimeZone.currentSystemDefault())
 
-val currentDay: LocalDate
-    get() = currentTime.date
+val LocalDateTime.mensaDay: LocalDate
+    get() = if (isAfterMensaHours) date + 1.days else date
+
+private val LocalDateTime.isAfterMensaHours
+    get() = hour >= 15
+
+private val Int.days: DatePeriod
+    get() = DatePeriod(days = this)
