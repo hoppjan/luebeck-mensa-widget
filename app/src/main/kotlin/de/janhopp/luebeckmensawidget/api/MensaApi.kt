9a9d61479cf14f8017011c9de440d7151350af42
hoppjan <83405332+hoppjan@users.noreply.github.com>
2023-10-18 11:20:36 +0200

    Moved Json (de)serializer object to its own file to reuse in API cache

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/api/MensaApi.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/api/MensaApi.kt
@@ -8,24 +8,18 @@ import de.janhopp.luebeckmensawidget.utils.toMensaApiFormat
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
-import kotlinx.serialization.json.Json
 import kotlin.coroutines.cancellation.CancellationException
 
 class MensaApi(
     private val client: HttpClient = HttpClient(),
 ) {
-    private val json = Json {
-        ignoreUnknownKeys = true
-        isLenient = true
-    }
-
     private val urlDay: String
         get() = currentTime.mensaDay.dayOfWeek.toMensaApiFormat()
 
     suspend fun getMealsToday(): List<MensaDay> = runCatching {
         client.get("https://speiseplan.mcloud.digital/meals?day=$urlDay")
             .body<String>()
-            .let { days -> json.decodeFromString<List<MensaDay>>(days) }
+            .let { days -> MensaJson.decodeFromString<List<MensaDay>>(days) }
     }.alsoThrow<_, CancellationException>() // needed for use of runCatching in coroutines
         .getOrDefault(listOf())
 }
