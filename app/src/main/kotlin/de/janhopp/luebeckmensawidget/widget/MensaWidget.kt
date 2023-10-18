3f904f8bb487acb7503b38fac2b0e3da2d761d9c
hoppjan <83405332+hoppjan@users.noreply.github.com>
2023-10-18 11:24:30 +0200

    Added naive use of API cache to widget

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/widget/MensaWidget.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/widget/MensaWidget.kt
@@ -5,21 +5,33 @@ import androidx.glance.GlanceId
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import de.janhopp.luebeckmensawidget.api.MensaApi
+import de.janhopp.luebeckmensawidget.storage.MenuStorage
 import de.janhopp.luebeckmensawidget.theme.AppTheme
 import de.janhopp.luebeckmensawidget.ui.MensaScreen
+import de.janhopp.luebeckmensawidget.utils.currentTime
+import de.janhopp.luebeckmensawidget.utils.mensaDay
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 
 class MensaWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         val api = MensaApi()
+        val storage = MenuStorage(context)
         withContext(Dispatchers.IO) {
             val meals = api.getMealsToday()
 
+            val today = if (meals.isEmpty()) {
+                storage.getMensaDay(currentTime.mensaDay).first()
+            } else {
+                storage.setMensaDays(meals)
+                meals.first()
+            }
+
             provideContent {
                 AppTheme {
-                    MensaScreen(meals)
+                    MensaScreen(today)
                 }
             }
         }
