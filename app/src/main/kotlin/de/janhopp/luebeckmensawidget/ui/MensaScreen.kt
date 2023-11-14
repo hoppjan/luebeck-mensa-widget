c2b96a36e7dd27900787f96f3685c60a25fcd3a7
hoppjan <83405332+hoppjan@users.noreply.github.com>
2023-11-14 13:37:05 +0100

    Added a small refresh button #8

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/MensaScreen.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/MensaScreen.kt
@@ -2,21 +2,37 @@ package de.janhopp.luebeckmensawidget.ui
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
 import androidx.glance.background
+import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
 import de.janhopp.luebeckmensawidget.api.model.MensaDay
 import de.janhopp.luebeckmensawidget.ui.components.StyledText
 
 @Composable
-fun MensaScreen(day: MensaDay?) {
+fun MensaScreen(
+    day: MensaDay?,
+    update: () -> Unit,
+) {
     Box(
         modifier = GlanceModifier
-            .background(MaterialTheme.colorScheme.background),
+            .background(MaterialTheme.colorScheme.background)
+            .fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd,
     ) {
         if (day != null)
             MensaDayView(day)
         else
-            StyledText(text = "Couldn't load menu...")
+            StyledText(
+                modifier = GlanceModifier
+                    .fillMaxSize() // goes to box-alignment if this is not set
+                    .padding(all = 8.dp),
+                text = "Couldn't load menu..."
+            )
+
+        RefreshButton(update)
     }
 }
