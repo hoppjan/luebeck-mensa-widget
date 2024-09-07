1bfdc1f345d3ecb5f40f7c6a7244bf747d80e9e6
Importantus <importantus@mailbox.org>
2024-09-07 21:42:00 +0200

    Use date instead of days and weeks for fetching meals

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/utils/Today.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/utils/Today.kt
@@ -2,6 +2,7 @@ package de.janhopp.luebeckmensawidget.utils
 
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
@@ -13,7 +14,14 @@ val currentTime: LocalDateTime
         .toLocalDateTime(TimeZone.currentSystemDefault())
 
 val LocalDateTime.mensaDay: LocalDate
-    get() = if (isAfterMensaHours) date + 1.days else date
+    get() = when {
+        date.dayOfWeek == DayOfWeek(6) -> date + 2.days
+        date.dayOfWeek == DayOfWeek(7) || isAfterMensaHours -> date + 1.days
+        else -> date
+    }
+
+val LocalDateTime.mensaApiFormat: String
+    get() = "%04d-%02d-%02d".format(mensaDay.year, mensaDay.monthNumber, mensaDay.dayOfMonth)
 
 private val LocalDateTime.isAfterMensaHours
     get() = hour >= 15
