dcece72f9117288fb138aea6bc667e68379f95b5
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-09-12 12:05:17 +0300

    Migrate plugin declarations to version catalogue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization") version "2.0.20"
-    kotlin("plugin.compose")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
