20c94ee0ce183e50a4e4e88db1c9cf4b8877a359
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-09-12 23:50:32 +0300

    Set result of configuration explicitly
    https://developer.android.com/develop/ui/views/appwidgets/configuration#update
    cannot set layout as glance has no such file

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/WidgetConfigurationActivity.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/WidgetConfigurationActivity.kt
@@ -1,5 +1,8 @@
 package de.janhopp.luebeckmensawidget
 
+import android.appwidget.AppWidgetManager.EXTRA_APPWIDGET_ID
+import android.appwidget.AppWidgetManager.INVALID_APPWIDGET_ID
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,18 +10,28 @@ import de.janhopp.luebeckmensawidget.ui.config.WidgetConfigurationScreen
 import de.janhopp.luebeckmensawidget.ui.theme.MensaTheme
 
 class WidgetConfigurationActivity : ComponentActivity() {
+    private var appWidgetId = INVALID_APPWIDGET_ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        appWidgetId = intent?.extras?.getInt(EXTRA_APPWIDGET_ID, INVALID_APPWIDGET_ID)
+            ?: INVALID_APPWIDGET_ID
 
         setContent {
             MensaTheme {
                 WidgetConfigurationScreen(
                     onConfigurationFinished = {
-                        setResult(RESULT_OK)
+                        setResult(RESULT_OK, Intent().putExtra(EXTRA_APPWIDGET_ID, appWidgetId))
                         finish()
                     }
                 )
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        setResult(RESULT_CANCELED, Intent().putExtra(EXTRA_APPWIDGET_ID, appWidgetId))
     }
 }
