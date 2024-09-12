a9b40a34286807ef1551bf79dd27a6b80f33fb95
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-09-12 12:40:50 +0300

    Bump app version to v0.0.5-7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.janhopp.luebeckmensawidget"
         minSdk = 29
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.0.4"
+        versionCode = 7
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
