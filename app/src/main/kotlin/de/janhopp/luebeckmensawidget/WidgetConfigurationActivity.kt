f8bdf29a134f062cbb5223a31b5a75579282be38
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-09-12 15:37:40 +0300

    Add composable to close the config Activity

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/WidgetConfigurationActivity.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/WidgetConfigurationActivity.kt
@@ -1,5 +1,29 @@
 package de.janhopp.luebeckmensawidget
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import de.janhopp.luebeckmensawidget.ui.config.WidgetConfigurationScreen
 
-class WidgetConfigurationActivity : ComponentActivity()
+class WidgetConfigurationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme(
+                colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+            ) {
+                WidgetConfigurationScreen(
+                    onConfigurationFinished = {
+                        setResult(RESULT_OK)
+                        finish()
+                    }
+                )
+            }
+        }
+    }
+}
