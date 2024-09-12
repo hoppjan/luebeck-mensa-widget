f81d660d9428b38b9d4d3d6438ee85964aa9f065
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-09-12 15:50:31 +0300

    Make config screen use material 3 style

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/config/WidgetConfigurationScreen.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/config/WidgetConfigurationScreen.kt
@@ -1,33 +1,50 @@
 package de.janhopp.luebeckmensawidget.ui.config
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WidgetConfigurationScreen(
     onConfigurationFinished: () -> Unit,
 ) {
-    Column(
+    Scaffold(
         modifier = Modifier.fillMaxSize()
             .background(MaterialTheme.colorScheme.background),
-    ) {
-        Text(
-            text = "Widget Configuration Screen",
-            style = MaterialTheme.typography.titleLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-        )
-        Button(
-            onClick = onConfigurationFinished,
-        ) {
-            Text(text = "Finish Configuration")
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = { Text(text = "Widget Configuration Screen") },
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = onConfigurationFinished,
+            ) {
+                Text(text = "Finish Configuration")
+            }
         }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        )
     }
 }
 
