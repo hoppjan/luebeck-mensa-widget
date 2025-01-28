19453e72a0ee7ded853f7a81817be7f88ac024d9
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-01-28 21:34:39 +0200

    Add warning for allergens to widget (#43)

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/MensaDayView.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/MensaDayView.kt
@@ -1,20 +1,30 @@
 package de.janhopp.luebeckmensawidget.ui
 
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
 import de.janhopp.luebeckmensawidget.R
 import de.janhopp.luebeckmensawidget.api.model.MensaDay
 import de.janhopp.luebeckmensawidget.api.model.filterDeals
+import de.janhopp.luebeckmensawidget.api.model.formatPrice
+import de.janhopp.luebeckmensawidget.api.model.getFor
 import de.janhopp.luebeckmensawidget.theme.glanceString
+import de.janhopp.luebeckmensawidget.theme.toGlance
+import de.janhopp.luebeckmensawidget.ui.components.StyledText
+import de.janhopp.luebeckmensawidget.ui.utils.glanceString
 import de.janhopp.luebeckmensawidget.widget.MensaWidgetConfig
 
 @Composable
 fun MensaDayView(day: MensaDay, widgetConfig: MensaWidgetConfig) {
-    val (showDate, useEmoji, priceGroup, filterDeals) = widgetConfig
+    val (showDate, useEmoji, priceGroup, filterDeals, _, allergens) = widgetConfig
+    val allergenCodes = allergens.map { it.code }
 
     Column {
         if (showDate)
@@ -33,7 +43,22 @@ fun MensaDayView(day: MensaDay, widgetConfig: MensaWidgetConfig) {
                     items = day.meals
                         .filterDeals(isEnabled = filterDeals)
                 ) { meal ->
-                    MealView(meal, useEmoji, priceGroup)
+                    Column(
+                        modifier = GlanceModifier.padding(8.dp)
+                    ) {
+                        StyledText(
+                            style = LocalTextStyle.current.toGlance()
+                                .copy(fontWeight = FontWeight.Bold),
+                            text = if (useEmoji) meal.widgetName else meal.name
+                        )
+                        if (meal.allergens.any { it.code in allergenCodes }) {
+                            val mealAllergens = meal.allergens.mapNotNull { it.glanceString() }
+                            StyledText(text = "⚠️ ${mealAllergens.joinToString(", ")}")
+                        }
+                        StyledText(
+                            text = meal.price.getFor(priceGroup).formatPrice()
+                        )
+                    }
                 }
             }
     }
