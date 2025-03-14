c72b19823cb62b6cfeb0d00f1e3988fafa8f1020
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-03-14 22:47:00 +0100

    Add language parameter to url

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/api/MensaApi.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/api/MensaApi.kt
@@ -1,5 +1,6 @@
 package de.janhopp.luebeckmensawidget.api
 
+import de.janhopp.luebeckmensawidget.api.model.Language
 import de.janhopp.luebeckmensawidget.api.model.Location
 import de.janhopp.luebeckmensawidget.api.model.MensaDay
 import de.janhopp.luebeckmensawidget.api.model.filterByDate
@@ -12,6 +13,7 @@ import de.janhopp.luebeckmensawidget.utils.mensaApiFormat
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import java.util.Locale
 import kotlin.coroutines.cancellation.CancellationException
 
 class MensaApi(
@@ -20,8 +22,14 @@ class MensaApi(
     private val date: String
         get() = currentTime.mensaApiFormat
 
+    private val lang: Language
+        get() = if (Locale.getDefault().language == Language.English.code)
+            Language.English
+        else
+            Language.German
+
     suspend fun getMealsToday(locations: Set<Location>): MensaDay? = runCatching {
-        client.get("https://speiseplan.mcloud.digital/v2/meals?location=${locations.toCodes()}&date=$date")
+        client.get("https://speiseplan.mcloud.digital/v2/meals?location=${locations.toCodes()}&date=$date&language=${lang.code}")
             .body<String>()
             .toApiResponse()
             .toMensaDays()
