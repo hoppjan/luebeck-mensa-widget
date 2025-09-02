f7bc9b01d4e27b3f7c0cca81e23eaa8f99f03537
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-09-02 20:29:35 +0200

    Enqueue the SyncWorker to enable it

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/activity/MensaDayActivity.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/activity/MensaDayActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import de.janhopp.luebeckmensawidget.SyncWorker.Companion.enqueueSyncWork
 import de.janhopp.luebeckmensawidget.ui.theme.MensaTheme
 
 class MensaDayActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        applicationContext.enqueueSyncWork()
         enableEdgeToEdge()
         setContent {
             MensaTheme {
