f1d7f062237928552b865927a4957ba72d9d2ee1
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-09-05 17:55:10 +0200

    Bump app version to 'v0.6.0-beta3'-22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "de.janhopp.luebeckmensawidget"
         minSdk = 29
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.6.0-beta2"
+        versionCode = 22
+        versionName = "0.6.0-beta3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
