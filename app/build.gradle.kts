af515148087fb6ee2e39df1bafd5acd3f7a35ce1
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-09-21 16:38:20 +0200

    Add AboutLibraries dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.aboutlibraries)
 }
 
 android {
@@ -86,4 +87,6 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
 
     implementation(libs.androidx.work.runtime.ktx)
+
+    implementation(libs.aboutlibraries.compose.m3)
 }
