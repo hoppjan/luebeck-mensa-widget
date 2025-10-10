e3bec24e2c6105aeb651757b03248afd70cd9d2b
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-10-10 17:03:10 +0200

    Migrate away from deprecated type

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/components/OptionDropdownMenu.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/components/OptionDropdownMenu.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -42,7 +42,7 @@ inline fun <reified T: Enum<T>> ColumnScope.OptionDropdownMenu(
                     modifier = Modifier
                         .fillMaxWidth(fraction = 0.6f)
                         .menuAnchor(
-                            type = MenuAnchorType.PrimaryEditable,
+                            type = ExposedDropdownMenuAnchorType.PrimaryEditable,
                             enabled = true,
                         ),
                     readOnly = true,
