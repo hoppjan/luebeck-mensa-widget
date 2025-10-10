eefa97fe583f8871f11c6cc4abff40654b11f2f6
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-10-10 15:55:37 +0200

    Bump Java version to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,11 +42,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+    kotlin {
+        jvmToolchain(21)
     }
     buildFeatures {
         compose = true
