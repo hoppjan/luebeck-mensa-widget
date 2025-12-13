1907c7391f5ca351b51baf66bf0947327ea82a81
Jan 🐢 <83405332+hoppjan@users.noreply.github.com>
2025-12-13 16:08:46 +0100

    Sync all data (#119)
    * Rename worker that only fetches meals for one day
    * Move buildConstraints to utils for reuse
    * Rename to differentiate
    * Add new sync worker that gets all data
    * Enqueue new sync worker

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/activity/MensaDayActivity.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/activity/MensaDayActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import de.janhopp.luebeckmensawidget.SyncWorker.Companion.enqueueSyncWork
+import de.janhopp.luebeckmensawidget.CompleteSyncWorker.Companion.enqueueCompleteSyncWork
+import de.janhopp.luebeckmensawidget.DaySyncWorker.Companion.enqueueDaySyncWork
 import de.janhopp.luebeckmensawidget.ui.theme.MensaTheme
 
 class MensaDayActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        applicationContext.enqueueSyncWork()
+        applicationContext.enqueueDaySyncWork()
+        applicationContext.enqueueCompleteSyncWork()
         WindowCompat.enableEdgeToEdge(window)
         setContent {
             MensaTheme {
