6c2f831699948d23f862591e501bd0418f025b91
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-12-14 16:47:33 +0100

    Remove unnecessary import

--- app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/activity/MensaMenuScreen.kt
+++ app/src/main/kotlin/de/janhopp/luebeckmensawidget/ui/activity/MensaMenuScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
